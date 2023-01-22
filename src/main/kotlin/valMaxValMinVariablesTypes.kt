765f56d2ffc962feb67bd5fc2a08e6fa41febeeb
iaalvearp <iaalvearp@gmail.com>
2023-01-22 12:28:54 -0500

    Playing a little with variable types

---
+++ src/main/kotlin/valMaxValMinVariablesTypes.kt
@@ -0,0 +1,35 @@
+
+fun main (){
+    val byteMinValue: Byte = Byte.MIN_VALUE
+    val byteMaxValue: Byte = Byte.MAX_VALUE
+    println("Min value Byte: $byteMinValue")
+    println("Max value Byte: $byteMaxValue")
+
+    val longMinValue: Long = Long.MIN_VALUE
+    val longMaxValue: Long = Long.MAX_VALUE
+    println("Min value Long: $longMinValue")
+    println("Max value Long: $longMaxValue")
+
+    val intMinValue: Int = Int.MIN_VALUE
+    val intMaxValue: Int = Int.MAX_VALUE
+    println("Min value Int: $intMinValue")
+    println("Max value Int: $intMaxValue")
+
+    val shortMinValue: Short = Short.MIN_VALUE
+    val shortMaxValue: Short = Short.MAX_VALUE
+    println("Min value Short: $shortMinValue")
+    println("Max value Short: $shortMaxValue")
+
+    val doubleMinValue: Double = Double.MIN_VALUE
+    val doubleMaxValue: Double = Double.MAX_VALUE
+    println("Min value Double: $doubleMinValue")
+    println("Max value Double: $doubleMaxValue")
+
+    val floatMinValue: Float = Float.MIN_VALUE
+    val floatMaxValue: Float = Float.MAX_VALUE
+    println("Min value Float: $floatMinValue")
+    println("Max value Float: $floatMaxValue")
+
+
+
+}
